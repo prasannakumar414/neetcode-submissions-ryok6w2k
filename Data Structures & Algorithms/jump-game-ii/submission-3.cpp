83a39164e00cf0aea17e1d0014c37bdafdf22a88
Prasanna kumar Reddi <86097533+prasannakumar414@users.noreply.github.com>
2026-07-03 18:57:11 +0530

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int size = nums.size();
+        int l = 0;
+        int r = 0;
+        int res = 0;
+        while(r < size-1) {
+            int longest = 0;
+            for(int i = l;i<r+1;i++) {
+                longest = max(i+nums[i], longest);
+            }
+            l = r;
+            r = longest;
+            res += 1;
+        }
+        return res;
+    }
+};
